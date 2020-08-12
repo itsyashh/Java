3fd608dda1b0d1f9acbd999a72096d4aacd213fe
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-12 18:03:15 +0530

    Add files via upload

---
+++ SpanArray.java
@@ -0,0 +1,28 @@
+package com.javacompany;
+
+import java.util.Scanner;
+
+public class SpanArray {
+    public static void main(String[]args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr [] = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] > max){
+                max = arr[1];
+            }else if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        int span = max - min;
+        System.out.println(span);
+
+    }
+}
