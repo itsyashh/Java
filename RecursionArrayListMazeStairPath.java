7b1356106b2a40562fca3807b6e157b3eaa093e6
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-22 20:04:47 +0530

    Add files via upload

---
+++ RecursionArrayListMazeStairPath.java
@@ -0,0 +1,40 @@
+package com.javacompany;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RecursionArrayListMazeStairPath {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        ArrayList<String> paths = getMazePaths(1,1,n, m);
+        System.out.println(paths);
+    }
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if( sr == dr && sc == dc){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        ArrayList<String> hpaths = new ArrayList<>();
+        ArrayList<String> vpaths = new ArrayList<>();
+
+        if( sc < dc){
+            hpaths = getMazePaths(sr, sc +1, dr, dc);
+        }
+        if(sr < dr){
+            vpaths = getMazePaths(sr +1, sc, dr, dc);
+        }
+        ArrayList<String> paths = new ArrayList<>();
+        for(String hpath : hpaths){
+            paths.add("h"+ hpath);
+        }
+        for(String vpath: vpaths){
+            paths.add('v' + vpath);
+        }
+        return paths;
+
+    }
+
+}
