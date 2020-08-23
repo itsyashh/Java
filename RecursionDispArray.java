a5f006b7396480d8c5cd74eaaf646eb03969c8e3
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-23 13:23:30 +0530

    Add files via upload

---
+++ RecursionDispArray.java
@@ -0,0 +1,24 @@
+package com.javacompany;
+
+import java.util.Scanner;
+
+public class RecursionDispArray {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        displayArr(arr, 0);
+
+    }
+    public static void displayArr(int[] arr, int idx){
+         if(idx == arr.length){
+             return;
+         }
+        System.out.println(arr[idx]);
+        displayArr(arr, idx+1);
+
+    }
+}
