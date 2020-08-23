d92bf5f4ce754d08fe69114ae001934dece5f537
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-23 13:23:52 +0530

    Add files via upload

---
+++ RecursionDispRev.java
@@ -0,0 +1,24 @@
+package com.javacompany;
+
+import java.util.Scanner;
+
+public class RecursionDispRev {
+    public static void main(String [] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        displayArrReverse(arr, 0);
+    }
+        public static void displayArrReverse(int[] arr, int idx) {
+        if(idx == arr.length){
+            return;
+        }
+
+        displayArrReverse(arr, idx+1);
+        System.out.println(arr[idx]);
+
+    }
+}
