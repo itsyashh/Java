5cbbd63af45090ca29a79f764d20d1adb5ec8540
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-24 13:37:40 +0530

    Add files via upload

---
+++ RecursionLastIndex.java
@@ -0,0 +1,31 @@
+package com.javacompany;
+
+import java.util.Scanner;
+
+public class RecursionLastIndex {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int d = scn.nextInt();
+        int fi = lastIndex(arr, 0, d);
+        System.out.println(fi);
+    }
+        public static int lastIndex(int[] arr, int idx, int x){
+            if(idx == arr.length){
+                return -1;
+            }
+            int liisa = lastIndex(arr, idx+1, x);
+
+            if(arr[idx] == x){
+                return idx;
+            }else {
+
+                return liisa;
+            }
+        }
+
+}
