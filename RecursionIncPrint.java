7fc60d98dd734527aa18cf39a34a2ab068234ba3
Yashwant Kumar <53390920+itsyashh@users.noreply.github.com>
2020-08-24 13:37:18 +0530

    Add files via upload

---
+++ RecursionIncPrint.java
@@ -0,0 +1,22 @@
+package com.javacompany;
+import java.util.Scanner;
+
+public class RecursionIncPrint {
+    public static void main(String[] args) throws Exception {
+        Scanner scn =new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+
+        if(n == 0){
+            return;
+        }
+        printIncreasing(n - 1);
+        System.out.println(n);
+
+    }
+
+}
+
